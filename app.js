06c1f6ab6436fb442e457b2b27b3b3a14a8d6745
Lydia Smith <l8smith@ucsd.edu>
2021-02-21 09:15:41 -0500

    added json to articles, connected education and articles

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ app.get('/', index.view);
 // app.get('/users', user.list);
 app.get('/education',education.viewEducation);
 app.get('/profile',profile.viewProfile);
-app.get('/articles/:title',articles.viewArticles);
+app.get('/articles/',articles.viewArticles);
+//app.get('/articles/:title',articles.viewArticles);
 app.get('/quiz',quiz.viewQuiz);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,8 @@
 /*
  * GET skin articles page.
  */
-// var data = require('data/articles.json');
-var data = require('/Users/kellyli/Desktop/CSE170/skinX/data/articles.json')
+var data = require('../data/articles.json');
+//var data = require('/Users/kellyli/Desktop/CSE170/skinX/data/articles.json')
 
 exports.viewArticles= function(req, res){
     //var title = req.params.title;
