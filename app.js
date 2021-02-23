7625ecd47b8691d37019237025927a4054c8b4c8
Lydia Smith <l8smith@ucsd.edu>
2021-02-22 20:25:30 -0600

    improved articles/education

--- app.js
+++ app.js
@@ -43,8 +43,7 @@ app.get('/', index.view);
 // app.get('/users', user.list);
 app.get('/education',education.viewEducation);
 app.get('/profile',profile.viewProfile);
-app.get('/articles/',articles.viewArticles);
-//app.get('/articles/:title',articles.viewArticles);
+app.get('/articles/:title',articles.viewArticles);
 app.get('/quiz',quiz.viewQuiz);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/education.js
+++ routes/education.js
@@ -2,9 +2,9 @@
 /*
  * GET skin education page.
  */
-
+var data = require('../data/articles.json');
 exports.viewEducation = function(req, res){
     console.log('opened education');
-    res.render('education');
+    res.render('education', data);
   };
   
